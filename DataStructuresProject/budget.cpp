a808599944e0ff7bccbca593f6e8d41ae538ca23
Muhannad Majed <mohanad.alfawzan1@gmail.com>
2024-04-27 18:27:45 +0300

    budget declariton

---
+++ DataStructuresProject/budget.cpp
@@ -0,0 +1,17 @@
+#include "global.h"
+
+bool checkBalance();
+
+void savings(); //Phase 3 and files manipluation
+
+// By hammad
+void modifySalary(double);  // change the salary data member
+
+// adding income and expnses -- By Bulaihi
+void addExpInc(double, string, bool); // use the checkBalance() method to give warnings
+
+// By AL-saif
+void summary();
+
+// By Eyad
+void removeExpense(int);
