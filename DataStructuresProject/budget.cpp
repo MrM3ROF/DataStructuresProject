d8cc7e932061823404fb4cf97d42f699ab0a194b
Muhannad Majed <mohanad.alfawzan1@gmail.com>
2024-04-27 19:20:08 +0300

    starting from here

--- DataStructuresProject/budget.cpp
+++ DataStructuresProject/budget.cpp
@@ -1,21 +1,29 @@
 #include "global.h"
 
-bool Budget::checkBalance();
-
-void Budget::savings(); //Phase 3 and files manipluation
-
 // By hammad
-void Budget::modifySalary(double);// change the salary data member
+bool Budget::checkBalance() {
 
+
+
+}
+
+void Budget::savings() { //Phase 3 and files manipluation
+
+
+}
+// By hammad
+void Budget::modifySalary(double) { // change the salary data member
+	
+}
 // adding income and expnses -- By Bulaihi
 void Budget::addExpInc(double, string, bool); // use the checkBalance() method to give warnings
 
 // By AL-saif
 void Budget::summary() {
-	cout << "hi";
+	
 }
 
 // By Eyad
 void Budget::removeExpense(int) {
-	cout << endl;
+	
 }
