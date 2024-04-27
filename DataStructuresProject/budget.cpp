85aebe62650577b21cc51af967763278a56c1606
MrM3ROF <117533277+MrM3ROF@users.noreply.github.com>
2024-04-27 21:29:45 +0300

    addExpInc function is done

--- DataStructuresProject/budget.cpp
+++ DataStructuresProject/budget.cpp
@@ -1,4 +1,5 @@
 #include "global.h"
+#include <cstring>
 
 // By hammad
 bool Budget::checkBalance() {
@@ -15,9 +16,32 @@ void Budget::savings() { //Phase 3 and files manipluation
 void Budget::modifySalary(double) { // change the salary data member
 	
 }
-// adding income and expnses -- By Bulaihi
-void Budget::addExpInc(double, string, bool) { // use the checkBalance() method to give warnings
 
+// adding income and expnses -- By Bulaihi
+void Budget::addExpInc(double value, string cat, bool type) { // use the checkBalance() method to give warnings
+    expense *temp  = new expense;
+    if(temp == NULL) {
+        cout<<"Storage is Full !!"<<endl;
+        return;
+    }
+    if(!type) value = value*(-1); //change the value according to its type income or expense
+    if (head == NULL){ //if the list is empty
+        temp->expenseID = 1;
+        head = tail = temp;
+    }
+    else {
+    temp->expenseID = tail->expenseID+1;
+    tail->next = temp;
+    tail=temp;
+    }
+    temp->next=NULL;
+    temp->category = cat;
+    temp->amount = value;
+    rBudget += value;
+
+    checkBalance();
+
+    
 }
 // By AL-saif
 void Budget::summary() {
