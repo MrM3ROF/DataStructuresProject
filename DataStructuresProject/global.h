fdefcea210408249aa6d64002276ffada3a79194
Muhannad Majed <mohanad.alfawzan1@gmail.com>
2024-04-27 18:17:54 +0300

    creating budget class and its methods

--- DataStructuresProject/global.h
+++ DataStructuresProject/global.h
@@ -1,4 +1,46 @@
-#ifndef global
+#ifndef GLOBAL_H
+#define GLOBAL_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Budget {
+
+private:
+	double rBudget;
+	double salary;
+	struct expense {
+		int expenseID;
+		double amount;
+		string category;
+		expense* next;
+	}*head, *tail;
+
+public:
+	Budget() {
+		head = tail = NULL;
+	}
+	// By hammad
+	bool checkBalance();
+
+	void savings(); //Phase 3 and files manipluation
+
+	// By hammad
+	void modifySalary(double);  // change the salary data member
+
+	// adding income and expnses -- By Bulaihi
+	void addExpInc(double, string, bool); // use the checkBalance() method to give warnings
+
+	// By AL-saif
+	void summary();
+
+	// By Eyad
+	void removeExpense(int); //
+
+	//void menu();
 
 
-#endif // !1
+};
+
+#endif // GLOBAL.h
