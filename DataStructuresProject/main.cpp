51f4b8ecf5c822121de4d05620c720c08719be03
Muhannad Majed <mohanad.alfawzan1@gmail.com>
2024-04-28 08:45:07 +0300

    adding modifyExpInc() to main

--- DataStructuresProject/main.cpp
+++ DataStructuresProject/main.cpp
@@ -6,6 +6,7 @@ int main() {
 	string stringInput;	// Saves any string to pass into methods
 	int choice;	// To save the choice from user in menu
 	Budget userBudget;
+	int choosenID;
 
 	cout << "Enter Your salary: ";
 	cin >> valueInput;
@@ -13,13 +14,14 @@ int main() {
 	userBudget.modifySalary(valueInput);
 
 	do {
-		cout << "Budget Tracker" << endl;
+		cout << "\tBudget Tracker" << endl;
 		cout << "1. Modify Salary" << endl;
 		cout << "2. Add Expense" << endl;
 		cout << "3. Add Income" << endl;
 		cout << "4. Display Summary" << endl;
 		cout << "5. Remove Last Expense/Income" << endl;
-		cout << "6. Exit" << endl;
+		cout << "6. Modify An Income/Expense By ID" << endl;
+		cout << "7. Exit" << endl;
 		cout << "Enter Your Choice: ";
 
 		cin >> choice;
@@ -51,15 +53,20 @@ int main() {
 		case 5:
 			userBudget.removeExpense();
 			break;
-		case 6:		// if choose 6(exit) then print summary before exit
+		case 6:
+			cout << "Enter ID of Income/Expense to modify: ";
+			cin >> choosenID;
+			userBudget.modifyExpInc(choosenID);
+			break;
+		case 7:		// if user choose 7 to exit then print summary before exit
 			// no break to print the summary
 		case 4:
 			userBudget.summary();
 			break;
-		default: // If user enter something not from (1-6) 
+		default: // If user enter something not from (1-7) 
 			cout << "Wrong input, Try again :)" << endl;
 			break;
 		}
 
-	} while (choice != 5);
+	} while (choice != 7);
 }
